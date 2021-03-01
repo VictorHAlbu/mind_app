1756ab8a0179513c3e8190003f34cbb3e711b35e
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2021-03-01 15:05:28 -0300

    Adicionando relacionamento N-N com Tag e Content(criado tabela TagContent

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,4 +2,7 @@ class Content < ApplicationRecord
   belongs_to :user
 
   validates :title, :description, presence: true, uniqueness: {case_sensitive: true}
+
+  has_many :tag_contents, dependent: :destroy
+  has_many :tags, through: :tag_contents
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_171952) do
+ActiveRecord::Schema.define(version: 2021_03_01_174253) do
 
   create_table "contents", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2021_03_01_171952) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_contents_on_user_id"
+  end
+
+  create_table "tag_contents", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "tag_id", null: false
+    t.bigint "content_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["content_id"], name: "index_tag_contents_on_content_id"
+    t.index ["tag_id"], name: "index_tag_contents_on_tag_id"
   end
 
   create_table "tags", charset: "utf8mb4", force: :cascade do |t|
@@ -43,5 +52,7 @@ ActiveRecord::Schema.define(version: 2021_03_01_171952) do
   end
 
   add_foreign_key "contents", "users"
+  add_foreign_key "tag_contents", "contents"
+  add_foreign_key "tag_contents", "tags"
   add_foreign_key "tags", "users"
 end
