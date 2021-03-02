f60afc4dbe90129946f1ef7ba20a504bcbcb1d52
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2021-03-01 21:42:59 -0300

    Adicionando edição ao conteudo

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -9,4 +9,23 @@ class ContentsController < ApplicationController
         @content = Content.new
     end
 
+    def create
+        @content = current_user.contents.build(content_params)
+
+        if @content.save
+            redirect_to contents_path, notice: 'Conteudo foi criado com sucesso'
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @content = Content.find(params[:id])
+    end
+
+    private
+    def content_params
+        params.require(:content).permit(:title, :description)
+    end
+    
 end
